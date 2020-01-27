67975934ff321903e1a309fd83aa7d50a6351400
Ricardo <Ricardofbdeoliveira@live.com>
2020-01-27 17:43:42 -0300

    Aula 11 - Conhecendo o Django Admin

--- projetoTeste/venvProjTest/meuPrimeiroProjeto/clientes/models.py
+++ projetoTeste/venvProjTest/meuPrimeiroProjeto/clientes/models.py
@@ -7,3 +7,5 @@ class Person(models.Model):
     salary = models.DecimalField(max_digits=5, decimal_places=2)
     bio = models.TextField()
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
