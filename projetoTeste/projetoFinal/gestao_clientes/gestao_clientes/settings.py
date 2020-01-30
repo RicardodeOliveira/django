9baaac1108fd548f434bff71830bf1aad1283919
Ricardo <Ricardofbdeoliveira@live.com>
2020-01-29 22:55:57 -0300

    Aula 28 - Banco postgres

--- projetoTeste/projetoFinal/gestao_clientes/gestao_clientes/settings.py
+++ projetoTeste/projetoFinal/gestao_clientes/gestao_clientes/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestao_clientes',
+        'USER': 'postgres',
+        'PASSWORD': 'syyjkk27',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
